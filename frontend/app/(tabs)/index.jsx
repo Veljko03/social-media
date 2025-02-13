6b7d7b9ee1cd6ad19606e1cb0f9e17d83c69c65c
Veljko03 <veljkodovragovic@gmail.com>
2025-02-13 16:51:26 +0100

    addded accelerometer for counting steps

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,17 +1,56 @@
-import { Link } from "expo-router";
+import { useState, useEffect, useRef } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Accelerometer } from "expo-sensors";
 
-export default function Index() {
+export default function App() {
+  const [{ x, y, z }, setData] = useState({ x: 0, y: 0, z: 0 });
+  const [subscription, setSubscription] = useState(null);
+  const [steps, setSteps] = useState(0);
+  const [lastMagnitude, setLastMagnitude] = useState(null);
+  const [threshold] = useState(1.3);
+  const lastStepTime = useRef(0);
+  const debounceTime = 300;
+
+  const _subscribe = () => {
+    Accelerometer.setUpdateInterval(16);
+    setSubscription(Accelerometer.addListener(setData));
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const magnitude = Math.sqrt(x * x + y * y + z * z);
+
+    if (lastMagnitude === null) {
+      setLastMagnitude(magnitude);
+      return;
+    }
+
+    if (magnitude > threshold && lastMagnitude <= threshold) {
+      const now = Date.now();
+      if (now - lastStepTime.current > debounceTime) {
+        setSteps((prevSteps) => prevSteps + 1);
+        lastStepTime.current = now;
+      }
+    }
+
+    setLastMagnitude(magnitude);
+  }, [x, y, z]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
-      <Link href="/about" style={styles.button}>
-        Go to about screena
-      </Link>
+      <Text style={styles.text}>Koraci: {steps}</Text>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,8 +62,6 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
   button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+    backgroundColor: "red",
   },
 });
